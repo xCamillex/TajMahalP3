1d73126651ea65ecd3fbba521605385830dc044d
virgile fantauzzi <virgilefantauzzi@gmail.com>
2023-09-04 23:35:29 +0200

    add javadoc, strings.xml and fonts

--- app/src/main/java/com/openclassrooms/tajmahal/di/AppModule.java
+++ app/src/main/java/com/openclassrooms/tajmahal/di/AppModule.java
@@ -10,10 +10,22 @@ import dagger.Provides;
 import dagger.hilt.InstallIn;
 import dagger.hilt.components.SingletonComponent;
 
+/**
+ * AppModule is responsible for providing application-level dependencies
+ * for the entire app. This module is installed in the SingletonComponent
+ * ensuring that the provided instances are retained throughout the app's lifecycle.
+ */
 @Module
 @InstallIn(SingletonComponent.class)
 public class AppModule {
 
+    /**
+     * Provides a singleton instance of the RestaurantApi. In this example,
+     * a fake implementation of the API is being used, which can be helpful
+     * during testing or mock scenarios.
+     *
+     * @return A singleton instance of the RestaurantFakeApi.
+     */
     @Provides
     @Singleton
     public RestaurantApi provideRestaurantApi() {
